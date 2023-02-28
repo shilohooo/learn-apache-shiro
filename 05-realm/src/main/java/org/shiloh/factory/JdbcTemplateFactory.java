16f9cc82ef8d5390de1f2666a48d9abc22175cf1
shiloh <lixiaolei595@gmail.com>
2023-02-28 23:18:53 +0800

    feat: JdbcTemplate 配置

--- 05-realm/src/main/java/org/shiloh/factory/JdbcTemplateFactory.java
+++ 05-realm/src/main/java/org/shiloh/factory/JdbcTemplateFactory.java
@@ -1,6 +1,11 @@
 package org.shiloh.factory;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * JdbcTemplate 工厂类
@@ -9,8 +14,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * @date 2023/2/28 18:39
  */
 public class JdbcTemplateFactory {
+    /**
+     * 全局 {@link JdbcTemplate} 实例
+     */
     private static JdbcTemplate jdbcTemplate;
 
+    /**
+     * 获取 {@link JdbcTemplate} 实例
+     *
+     * @return {@link JdbcTemplate} 实例
+     * @author shiloh
+     * @date 2023/2/28 22:16
+     */
     public static JdbcTemplate getInstance() {
         if (jdbcTemplate == null) {
             jdbcTemplate = createJdbcTemplate();
@@ -19,7 +34,34 @@ public class JdbcTemplateFactory {
         return jdbcTemplate;
     }
 
+    /**
+     * 创建 {@link JdbcTemplate} 实例，设置数据源
+     *
+     * @return {@link JdbcTemplate} 实例
+     * @author shiloh
+     * @date 2023/2/28 22:16
+     */
     private static JdbcTemplate createJdbcTemplate() {
-        return null;
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        try (
+                final InputStream inputStream = JdbcTemplateFactory.class
+                        .getClassLoader()
+                        .getResourceAsStream("jdbc.properties")
+        ) {
+            final Properties properties = new Properties();
+            properties.load(inputStream);
+
+            final DruidDataSource dataSource = new DruidDataSource();
+            dataSource.setDriverClassName(properties.getProperty("jdbc.driver-class-name"));
+            dataSource.setUrl("jdbc.url");
+            dataSource.setUsername("jdbc.username");
+            dataSource.setPassword("jdbc.password");
+
+            jdbcTemplate.setDataSource(dataSource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return jdbcTemplate;
     }
 }
