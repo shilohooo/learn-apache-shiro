c7ee33671225776084c0f843837ca0d7b79eb349
shiloh <lixiaolei595@gmail.com>
2023-03-27 23:34:38 +0800

    test: 密码加密单元测试

---
+++ 17-dynamic-url/src/test/java/org/shiloh/test/PasswordTests.java
@@ -0,0 +1,28 @@
+package org.shiloh.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.shiloh.utils.PasswordUtils;
+
+/**
+ * @author shiloh
+ * @date 2023/3/27 22:48
+ */
+@Slf4j
+public class PasswordTests {
+    /**
+     * 密码加密测试
+     *
+     * @author shiloh
+     * @date 2023/3/27 22:58
+     */
+    @Test
+    public void test() {
+        final String plaintext = "123456";
+        log.info("password: {}", plaintext);
+        final String salt = PasswordUtils.generateSalt();
+        log.info("salt: {}", salt);
+        final String encryptedPassword = PasswordUtils.encrypt(plaintext, salt);
+        log.info("encryptedPassword: {}", encryptedPassword);
+    }
+}
