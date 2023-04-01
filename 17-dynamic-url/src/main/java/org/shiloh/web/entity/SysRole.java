40f78eb77ce9e0466f34a091c860987ae803984d
shiloh <lixiaolei595@gmail.com>
2023-04-01 23:39:21 +0800

    refactor: 实现 RowMapper

--- 17-dynamic-url/src/main/java/org/shiloh/web/entity/SysRole.java
+++ 17-dynamic-url/src/main/java/org/shiloh/web/entity/SysRole.java
@@ -3,6 +3,10 @@ package org.shiloh.web.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.shiloh.web.entity.base.BaseEntity;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * 系统角色实体
@@ -12,7 +16,7 @@ import org.shiloh.web.entity.base.BaseEntity;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class SysRole extends BaseEntity {
+public class SysRole extends BaseEntity implements RowMapper<SysRole> {
     private static final long serialVersionUID = -5887319132521022506L;
 
     /**
@@ -29,4 +33,22 @@ public class SysRole extends BaseEntity {
      * 是否有效
      */
     private Boolean available = Boolean.TRUE;
+
+    /**
+     * Spring Jdbc RowMapper 映射实现，将表中的行映射为数据
+     *
+     * @param rs     查询结果集
+     * @param rowNum 当前行的行号
+     * @author shiloh
+     * @date 2023/4/1 23:23
+     */
+    @Override
+    public SysRole mapRow(ResultSet rs, int rowNum) throws SQLException {
+        final SysRole sysRole = new SysRole();
+        sysRole.setId(rs.getLong("id"));
+        sysRole.setName(rs.getString("name"));
+        sysRole.setParentId(rs.getLong("parent_id"));
+        sysRole.setAvailable(rs.getBoolean("available"));
+        return sysRole;
+    }
 }
